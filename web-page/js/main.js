cba45956e004866292967ee02e19a2ae54b9bbb0
Arkady <insite1029@gmail.com>
2020-05-03 16:37:37 +0300

    Web-cam

---
+++ web-page/js/main.js
@@ -0,0 +1,18 @@
+// web-cam code
+(function () {
+  let video = document.getElementById('video');
+
+  navigator.getMedia = navigator.getUserMedia ||
+    navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia ||
+    navigator.msGetUserMedia;
+
+  navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: false
+    })
+    .then(stream => {
+      video.srcObject = stream;
+      video.play();
+    });
+})();
