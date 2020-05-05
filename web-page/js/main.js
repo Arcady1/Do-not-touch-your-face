64cf39414d09a09237c319ab836e9e9bc95411a5
Arkady <insite1029@gmail.com>
2020-05-05 14:48:41 +0300

    Segmentation-BodyParts

--- web-page/js/main.js
+++ web-page/js/main.js
@@ -3,10 +3,11 @@ let canvas, video, ctx, imgToServer, imgData;
 
 canvas = document.getElementById('canvas');
 video = document.getElementById('video');
+image = document.getElementById('image');
 
 if (canvas.getContext('2d')) {
   ctx = canvas.getContext('2d');
-  ctx.scale(0.3, 0.3);
+  // ctx.scale(0.3, 0.3);
   videolink();
 } else
   alert('You browser does not support canvas.getContext("2d")');
@@ -37,7 +38,7 @@ function videolink() {
 }
 
 function Photo() {
-  ctx.drawImage(video, 180, 0);
+  // ctx.drawImage(video, 180, 0);
 
   // РАСКОММЕНТИРОВАТЬ! imgData получает URL скриншотов
   // imgData = canvas.toDataURL('image/jpeg', 0.5);
@@ -51,13 +52,33 @@ function Photo() {
       quantBytes: 2
     });
 
-    const segmentation = await net.segmentPerson(canvas, {
-      flipHorizontal: false,
+    console.log('Yes1');
+
+    const segmentation = await net.segmentPersonParts(image, {
+      flipHorizontal: true,
       internalResolution: 'medium',
       segmentationThreshold: 0.7
     });
 
+    console.log('Yes2');
+
+    const coloredPartImage = bodyPix.toColoredPartMask(segmentation);
+    const opacity = 0.7;
+    const flipHorizontal = true;
+    const maskBlurAmount = 0;
+
+    bodyPix.drawMask(
+      canvas, image, coloredPartImage, opacity, maskBlurAmount,
+      flipHorizontal);
+
     console.log(segmentation);
   }
   loadAndPredict();
+
+
 }
+
+// function f1() {
+//   let audio = document.getElementById('sound');
+//   audio.play();
+// }
