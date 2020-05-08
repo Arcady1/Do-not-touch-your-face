e5ce6fd12d7cf61fe771d2c3df85446faf718ebd
Arkady <insite1029@gmail.com>
2020-05-08 20:28:37 +0300

    Prototype

--- web-page/js/main.js
+++ web-page/js/main.js
@@ -93,7 +93,7 @@ function Photo() {
       const something = searching(arr_face, arr_palms);
       console.log(something);
 
-      if (something == 'found!')
+      if (something == 'Yes!')
         audio.play();
     }
     // 
@@ -101,49 +101,97 @@ function Photo() {
       const something = searching(arr_palms, arr_face);
       console.log(something);
 
-      if (something == 'found!')
+      if (something == 'Yes!')
         audio.play();
     };
 
     // поиск пересечения элементотв массивов
     function searching(arr_1, arr_2) {
-      console.log("Began...");
-      let num, pos, length, min_dist;
+      console.log("Searching is started");
+      let num, pos, old_pos, pre_old, length, center, min_dist;
 
-      min_dist = 10;
+      min_dist = 100;
       length = arr_1.length;
+      center = parseInt(length / 2);
+
+      // проверка, что массивы в принципе имеют точки пересечения
+      if (arr_2[0] > arr_1[length - 1])
+        return ('No-cross!');
+      // 
+      else if (arr_2[length - 1] < arr_1[0])
+        return ('No-cross!');
+
+      console.log('crossed-somewhere');
 
       for (let digit = 0; digit < arr_2.length; digit++) {
         num = arr_2[digit];
-        pos = parseInt(length / 2);
 
-        if (Math.abs(num - arr_1[0]) < min_dist)
-          return ('found!');
+        pos = center;
+        old_pos = pos;
+        pre_old = -1;
 
-        else if (Math.abs(num - arr_1[length - 1]) < min_dist)
-          return ('found!');
+        if (num == arr_1[0])
+          return ('Yes!');
 
-        else {
-          while (pos != 0) {
-            if (pos == (length - 1))
-              break;
+        else if (num == arr_1[length - 1])
+          return ('Yes!');
+
+        else if (num == arr_1[center])
+          return ('Yes!');
+
+        while (old_pos != pre_old) {
+          if (num > arr_1[pos]) {
+
+            if (pos >= center) {
+              pre_old = old_pos;
+              old_pos = pos;
+              pos += parseInt((length - pos) / 2);
+
+              if (Math.abs(arr_1[pos] - num) < min_dist) {
+                // alert(num + '&' + arr_1[pos]);
+                return ('Yes!');
+              }
+            }
             // 
-            else if (Math.abs(num - arr_1[pos]) < min_dist)
-              return ('found!');
+            else if (pos < center) {
+              pre_old = old_pos;
+              old_pos = pos;
+              pos += parseInt((center - pos) / 2);
+
+              if (Math.abs(arr_1[pos] - num) < min_dist) {
+                // alert(num + '&' + arr_1[pos]);
+                return ('Yes!');
+              }
+            }
+          }
+          //  
+          else if (num < arr_1[pos]) {
+            if (pos > center) {
+              pre_old = old_pos;
+              old_pos = pos;
+              pos -= parseInt((pos - center) / 2);
+
+              if (Math.abs(arr_1[pos] - num) < min_dist) {
+                // alert(num + '&' + arr_1[pos]);
+                return ('Yes!');
+              }
+            }
             // 
-            else if (num < arr_1[pos])
-              pos = parseInt(pos / 2);
-            // 
-            else if (num > arr_1[pos]) {
-              pos += (parseInt((length - pos) / 2));
-              if (arr_1[pos] > num)
-                break;
+            else if (pos <= center) {
+              pre_old = old_pos;
+              old_pos = pos;
+              pos -= parseInt(pos / 2);
+
+              if (Math.abs(arr_1[pos] - num) < min_dist) {
+                // alert(num + '&' + arr_1[pos]);
+                return ('Yes!');
+              }
             }
           }
         }
       }
 
-      return ('NOTfound!');
+      return ('No-end!');
     };
 
     // свойства маски
@@ -158,7 +206,6 @@ function Photo() {
       flipHorizontal);
 
     console.log(segmentation);
-    // console.log(arr_palms);
   }
 
   loadAndPredict();
